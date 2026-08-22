051baa9be3042b6621443b8e5c6bda8b83e0aba8
Hritviz Manral <hritvizmanral66@gmail.com>
2026-08-23 02:48:00 +0530

    Updated main.py with resume chain

--- job_resume_assistant/main.py
+++ job_resume_assistant/main.py
@@ -1,5 +1,5 @@
 from chains.job_chain import job_chain
-
+from chains.resume_chain import resume_chain
 
 job_description = input(
     "Paste the job description:\n"
@@ -10,23 +10,43 @@ response = job_chain.invoke({
     "job_description": job_description
 })
 
+# resume_text = """
 
-print("\n--- JOB ANALYSIS ---")
+# Name: Alex
 
-print("\nJob Title:")
-print(response.job_title)
+# Education:
+# B.Tech in Computer Science
 
-print("\nRequired Skills:")
-print(response.required_skills)
+# Projects:
+# Built a RAG chatbot
 
-print("\nPreferred Skills:")
-print(response.preferred_skills)
+# Certifications:
+# AWS Certified Cloud Practitioner
+# """
 
-print("\nResponsibilities:")
-print(response.responsibilities)
+# response = resume_chain.invoke({
+#     "resume": resume_text
+# })
 
-print("\nExperience:")
-print(response.experience_level)
+print(response)
 
-print("\nEducation:")
-print(response.education)
+
+# print("\n--- JOB ANALYSIS ---")
+
+# print("\nJob Title:")
+# print(response.job_title)
+
+# print("\nRequired Skills:")
+# print(response.required_skills)
+
+# print("\nPreferred Skills:")
+# print(response.preferred_skills)
+
+# print("\nResponsibilities:")
+# print(response.responsibilities)
+
+# print("\nExperience:")
+# print(response.experience_level)
+
+# print("\nEducation:")
+# print(response.education)
