d589652db1570fe8e0d9c789f9458a7e2e2a6c89
Hritviz Manral <hritvizmanral66@gmail.com>
2026-09-09 20:12:49 +0530

    Added new file

---
+++ job_resume_assistant/memory_inspection_demo.py
@@ -0,0 +1,40 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class State(TypedDict):
+    messages: list[str]
+
+
+def add_message(state: State):
+    return {
+        "messages": state["messages"] + ["New message added"]
+    }
+
+
+builder = StateGraph(State)
+
+builder.add_node("add_message", add_message)
+builder.add_edge(START, "add_message")
+builder.add_edge("add_message", END)
+
+memory = MemorySaver()
+graph = builder.compile(checkpointer=memory)
+
+config = {
+    "configurable": {
+        "thread_id": "user_1"
+    }
+}
+
+graph.invoke(
+    {"messages": ["Hello"]},
+    config=config
+)
+
+saved_state = graph.get_state(config)
+
+print(saved_state.values)
